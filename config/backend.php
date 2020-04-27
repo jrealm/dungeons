cf6dc4c931d2789d32bced99cbebdb8c3cc4df83
jrealm <jrealm@gmail.com>
2020-04-27 18:02:57 +0800

    .

--- config/backend.php
+++ config/backend.php
@@ -6,13 +6,13 @@ return [
     'delete.cancel.button' => 'btn-default',
     'delete.icon' => 'fas fa-trash-alt',
 
-    'edit.button' => 'btn-primary',
+    'edit.button' => 'btn-info',
     'edit.cancel.button' => 'btn-default',
     'edit.icon' => 'fas fa-edit',
 
     'menus' => 'authority|system',
 
-    'multiple-delete.button' => 'btn-app',
+    'multiple-delete.button' => 'btn-app text-danger',
 
     'new.button' => 'btn-app',
     'new.cancel.button' => 'btn-default',
