29b722d193cea7f3ddb14e72a1bc30d77a4c1479
jrealm <jrealm@gmail.com>
2020-07-03 11:08:45 +0800

    -

--- class/dungeons/service/Telegram.php
+++ class/dungeons/service/Telegram.php
@@ -7,7 +7,7 @@ class Telegram {
     public function execute($args) {
         $text = urlencode($args['text']);
 
-        @file_get_contents("https://api.telegram.org/bot{$args['bot']}/sendMessage?chat_id={$args['group']}&text={$text}");
+        @file_get_contents("https://api.telegram.org/bot{$args['bot']}/sendMessage?chat_id={$args['group']}&parse_mode=HTML&text={$text}");
     }
 
 }
