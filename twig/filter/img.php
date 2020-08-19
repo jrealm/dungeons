4a716d13ed19459eba5b58219cdc53a22a81ba0b
jrealm <jrealm@gmail.com>
2020-08-19 10:34:01 +0800

    -

--- twig/filter/img.php
+++ twig/filter/img.php
@@ -1,8 +1,19 @@
 <?php //>
 
-return new Twig\TwigFilter('img', function ($image) {
+return new Twig\TwigFilter('img', function ($image, $width = 0) {
     if (preg_match('/^data:/', $image)) {
         return $image;
+    }
+
+    if ($width) {
+        $folder = APP_HOME . 'www/files/';
+        $thumb = "{$image}_{$width}";
+
+        if (!file_exists($folder . $thumb)) {
+            exec("convert \"{$folder}{$image}\" -resize {$width}\\> \"{$folder}{$thumb}\"");
+        }
+
+        return APP_PATH . 'files/' . $thumb;
     }
 
     return APP_PATH . 'files/' . $image;
