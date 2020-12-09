240b0c7c4c36f798d72e96545979a1f0db935205
jrealm <jrealm@gmail.com>
2020-12-09 13:32:45 +0800

    -

--- class/dungeons/service/MitakeSms.php
+++ class/dungeons/service/MitakeSms.php
@@ -7,7 +7,11 @@ class MitakeSms {
     public function execute($args) {
         $args['phone'] = $args['prefix'] . ltrim($args['phone'], '0');
 
-        $response = file_get_contents(render($args['url'], $args));
+        if ($args['key'] === '00000') {
+            $response = 'statuscode=1';
+        } else {
+            $response = file_get_contents(render($args['url'], $args));
+        }
 
         if (preg_match('/statuscode=(\d+)/', $response, $matches) && $matches[1] < 5) {
             model('SmsLog')->insert(['receiver' => $args['phone'], 'content' => $args['text'], 'response' => $response]);
